4d4348cf4aa651d4a39133c8dba0724bfd98931d
evans123456 <evanpersie3@gmail.com>
2019-08-09 18:23:08 +0300

    changed the frontend

--- grossbox/bizzzzz/views.py
+++ grossbox/bizzzzz/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'bizzzzz/index.html')
+
+def list_of_business(request):
+    # return render(request, '')
+    pass
